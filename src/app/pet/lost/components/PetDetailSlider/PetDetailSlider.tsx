921742ead380feaf7daabbfabac58c8e04bc91a9
Juan Josue Mancilla <jujomago@gmail.com>
2025-11-10 18:58:44 -0400

    added commands

--- src/app/pet/lost/components/PetDetailSlider/PetDetailSlider.tsx
+++ src/app/pet/lost/components/PetDetailSlider/PetDetailSlider.tsx
@@ -19,45 +19,9 @@ export const PetDetailSlider = ({ images }: PetSliderProps) => {
         <SwiperSlide key={image.id}>
           <div className="relative h-[270px] w-full bg-gray-700 grid place-items-center">
             <PetImage url={image.url} alt="Fotitos" />
-            {/*          <Image
-              loader={cloudinaryLoader}
-              src={image.url}
-              alt={""}
-              fill
-              sizes="(max-width: 768px) 100vw, 500px"
-              className="object-contain" // o object-cover según prefieras
-            /> */}
           </div>
         </SwiperSlide>
       ))}
-
-      {/* <SwiperSlide>
-        <Image
-          src={imageSrc}
-          alt=""
-          className="object-cover relative z-0"
-          width={500}
-          height={500}
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          src={imageSrc}
-          alt=""
-          className="object-cover relative z-0"
-          width={500}
-          height={500}
-        />
-      </SwiperSlide>{" "}
-      <SwiperSlide>
-        <Image
-          src={imageSrc}
-          alt=""
-          className="object-cover relative z-0"
-          width={500}
-          height={500}
-        />
-      </SwiperSlide> */}
     </Swiper>
   );
 };
